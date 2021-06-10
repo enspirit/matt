b88d563dd690b950bed2a42c0d8cbb7a83192d3c
Bernard Lambeau <blambeau@gmail.com>
2021-06-10 16:37:05 +0200

    Bump to 1.1.1 and release.

--- lib/matt/version.rb
+++ lib/matt/version.rb
@@ -2,7 +2,7 @@ module Matt
   module Version
     MAJOR = 1
     MINOR = 1
-    TINY  = 0
+    TINY  = 1
   end # module Version
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].join(".")
 end # module Matt
